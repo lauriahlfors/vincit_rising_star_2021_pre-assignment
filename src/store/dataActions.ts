57f61da9fc4161b60907692564b7406998f7e430
Lauri Ahlfors <70107120+lauriahlfors@users.noreply.github.com>
2022-01-03 20:44:53 +0200

    update: delete old code

--- src/store/dataActions.ts
+++ src/store/dataActions.ts
@@ -22,34 +22,3 @@ export const updateData = (
   );
 };
 
-// export const updatePrices = (apiData: IData, data: IData): IData => {
-//   console.log('hello');
-//   return data.prices.map((item, index) => ({
-//     ...item,
-//     index: index,
-//     unix: item.unix,
-//     value: item.value,
-//   }));
-
-//   // data.prices.map(([unix, value], index) => ({
-//   //   index: index,
-//   //   unix: unix,
-//   //   value: value,
-//   // }));
-
-//   // setMarketCaps(
-//   //   data.market_caps.map(([unix, value], index) => ({
-//   //     index,
-//   //     unix,
-//   //     value,
-//   //   }))
-//   // );
-
-//   // setTotalVolumes(
-//   //   data.total_volumes.map(([unix, value], index) => ({
-//   //     index,
-//   //     unix,
-//   //     value,
-//   //   }))
-//   // );
-// };
